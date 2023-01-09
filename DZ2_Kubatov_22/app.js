11afb785e1a1ea34433fea67d08dbe096667d58a
Sultan <119931354+Kubatov88@users.noreply.github.com>
2023-01-09 16:35:49 +0600

    Add files via upload

---
+++ DZ2_Kubatov_22/app.js
@@ -0,0 +1,61 @@
+//////////////////////////// DZ1
+
+
+
+//  var arr = [10, 20, 30, 50, 235, 3000]
+//  console.log(arr["0"], arr["1"], arr["3"]);
+
+
+
+
+/////////////////////////////// DZ2
+
+//   let num = 20;
+//   while (num >= 0) {
+//     console.log(num);
+//     num--;
+//   }
+
+
+
+
+
+// for (let num = 20; num >= 0; num--) {
+//     console.log(num);
+// }
+
+
+//////////////////////////// DZ3
+
+// const traffic = prompt("Введите цвет светофора!")
+// switch (traffic) {
+//     case "red":
+//     case "красный":   
+//     console.log("Стоп");
+//       break;
+//     case "yellow":
+//     case "желтый":    
+//       console.log("Жди ");
+//       break;
+//     case "green":
+//     case "зеленый":
+//         console.log("Иди");   
+//       break;
+//     default:
+//       break;  
+// }
+
+
+
+// const traffic = prompt("Введите цвет светофора");
+// if(traffic === "красный" || traffic === "red") console.log("Стой");
+// else if(traffic === "желтый" || traffic === "yellow") console.log("Жди");
+// else if(traffic === "зеленый" || traffic === "green") console.log("Иди");
+// else console.log("Ошибка");
+
+
+
+
+
+
+
